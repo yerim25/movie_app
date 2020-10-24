0772cc9a9e8cab5a6d41b3e026a976be7889edc6
Kang Yerim <yerim25@gmail.com>
2020-10-24 19:34:04 +0900

    Updates

---
+++ precache-manifest.551618edeea3b228d1c5921044ac36bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bcff499aac980ffd35245a8942ce9e",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2be0ca433a1c5d1d1e00",
+    "url": "/movie_app/static/css/main.0d3fe85e.chunk.css"
+  },
+  {
+    "revision": "25fd20164c507f35b04f",
+    "url": "/movie_app/static/js/2.b6b5f07a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/movie_app/static/js/2.b6b5f07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be0ca433a1c5d1d1e00",
+    "url": "/movie_app/static/js/main.81277c66.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
